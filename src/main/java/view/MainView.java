4d6221c231b1ea62210435f02afbaa27ec728f7f
CarlosEduardoL <carlos.lizalda1@correo.icesi.edu.co>
2020-11-14 09:43:56 -0500

    javafx setup

---
+++ src/main/java/view/MainView.java
@@ -0,0 +1,16 @@
+package view;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class MainView extends Application {
+    public static void main(String[] args) {
+        Application.launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        primaryStage.setTitle("CryptoFiles");
+        primaryStage.show();
+    }
+}
